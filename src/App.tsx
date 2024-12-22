5f04dbaac75d92be5a23cb0a1fb6ab7c30078d5b
heyakash876 <119721777+heyakash876@users.noreply.github.com>
2024-12-22 18:49:57 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Projects from "./components/projects/Projects";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+//import Hackathons from "./components/hackathons/Hackathons";
+import Navbar from "./components/navbar/Navbar";
+import Terminal from "./components/terminal/Terminal";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <About />
+      <Skills />
+      {/* <Hackathons /> */}
+      <Projects />
+      <Terminal />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
