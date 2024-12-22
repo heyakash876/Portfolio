641a2a560d6b3c505273746a887c6290a0a62b5d
heyakash876 <119721777+heyakash876@users.noreply.github.com>
2024-12-22 18:55:52 +0530

    init

---
+++ src/helpers/text.ts
@@ -0,0 +1,45 @@
+//import { hackathonsEn } from "../components/hackathons/config";
+import { projectsEn } from "../components/projects/config";
+import { skillsEn } from "../components/skills/config";
+
+export const text = {
+  "en": {
+    // projetcs page
+    projects: projectsEn,
+    projectsHeading: "Projects",
+    projectsDescription: "Take a look at my blockchain projects:",
+    projectsGithubText: "You can find my other projects on my GitHub",
+    projectsMoreText: " . . . more coming soon!",
+    //projectsViewMore: "View More",
+    //projectsViewLess: "View Less",
+
+    // skillset page
+    skills: skillsEn,
+    skillsetHeading: "Skillset",
+    skillsetDescription: "Technologies I use in my development:",
+
+    // // hackathons page
+    // hackathons: hackathonsEn,
+    // hackathonsHeading: "Hackathons",
+    // hackathonsDescription: "In a little less than a year I have won awards at 3 blockchain hackathons",
+    // hackathonsProjectText: "Project: ",
+    // hackathonViewHackathon: "View Hackathon",
+
+    // contact page
+    contactHeading: "Contact Me",
+
+    // about page
+    aboutHi: "Hi! 👋",
+    aboutIam: "I'am ",
+    aboutSkilled: "skilled",
+    aboutText: " with  expertise in blockchain development, enthusiastic about blockchain and web3 industry, constantly seeking opportunities for continuous learning and growth.",
+
+    // others
+    contactButton: "Contact Me",
+
+    // terminal page
+    terminalHeading: "Terminal",
+    terminalDescription: "Use this interactive terminal to find out more about me and my work!"
+  },
+ 
+};
